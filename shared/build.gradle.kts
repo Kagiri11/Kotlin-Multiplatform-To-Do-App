e08773500802e6040327845ad3cb74fddf7ec7f0
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2024-08-13 23:37:45 +0300

    sets up dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.room)
 }
 
 kotlin {
@@ -25,12 +29,31 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-            //put your multiplatform dependencies here
+            implementation(libs.androidx.datastore)
+            implementation(libs.androidx.datastore.preferences.core)
+
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.materialIconsExtended)
+            implementation(compose.components.resources)
+
+            implementation(libs.material3.window.size)
+
+            implementation(libs.room.runtime)
+            implementation(libs.sqlite.bundled)
+
+            api(libs.koin.core)
+            implementation(libs.kotlin.coroutines.core)
+            implementation(libs.koin.composeVM)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
     }
+}
+
+room {
+    schemaDirectory("$projectDir/schemas")
 }
 
 android {
@@ -43,4 +66,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+}
+
+dependencies {
+    ksp(libs.room.compiler)
 }
